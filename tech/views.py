662b6b21dbc69a26c0ecfae3f75e28e293b7612f
theilorsilva <martins.theilor8@gmail.com>
2025-01-08 09:25:31 -0300

    Implementação Django Allauth e registro da aplicação

---
+++ tech/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def members(request):
+    return render(request, 'members.html')
